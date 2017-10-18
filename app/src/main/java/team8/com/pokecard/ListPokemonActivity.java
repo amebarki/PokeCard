dd25138e1fd5b5fddc8caacea9244d6d2a1aa000
adam <adam.mebarki@etu.univ-lyon1.fr>
2017-10-18 12:29:24 +0200

    Initial commit

---
+++ app/src/main/java/team8/com/pokecard/ListPokemonActivity.java
@@ -0,0 +1,13 @@
+package team8.com.pokecard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListPokemonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_pokemon);
+    }
+}
