6bf8ede062ff0fe23dba30aeec81e1df84706e57
iem <iem@iutabgdin202-17.univ-lyon1.fr>
2017-11-15 09:07:06 +0100

    Login Fini

--- app/src/main/java/team8/com/pokecard/GoogleSignIn.java
+++ app/src/main/java/team8/com/pokecard/GoogleSignIn.java
@@ -1,8 +1,40 @@
 package team8.com.pokecard;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 /**
  * Created by iem on 10/11/2017.
  */
 
-public class GoogleSignIn {
+public class GoogleSignIn implements GoogleApiClient.OnConnectionFailedListener {
+    GoogleApiClient mGoogleApiClient;
+    Context context;
+
+    public GoogleSignIn(FragmentActivity fragmentActivity) {
+        this.context = fragmentActivity.getBaseContext();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .enableAutoManage(fragmentActivity,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(context, "onConnectionFailed", Toast.LENGTH_SHORT).show();
+    }
+
 }
